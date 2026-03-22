80772bbf822203be3e2a167b5b140eb90e89a41c
Keshav Kumar <130147146+Keshav202@users.noreply.github.com>
2026-03-22 22:16:24 +0530

    Add SubArrays class to print all subarrays

---
+++ Arrays/07 SubArrays.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Sub_Arrays {
+
+    public static void sub_array(int array[]){
+        for (int i=0; i< array.length; i++){
+            int start = i;
+            for (int j=i; j< array.length; j++){
+                int end = j;
+                System.out.print("[ ");
+                for (int k=start; k<=end; k++){
+                    System.out.print(array[k] + " ");
+                }
+                System.out.println("]");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int array[] = new int[5];
+
+        System.out.println("Enter elements in Array : ");
+        for (int i = 0; i < array.length; i++){
+            array[i] = sc.nextInt();
+        }
+
+        sub_array(array);
+
+    }
+}
